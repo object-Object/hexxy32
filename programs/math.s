0c955b8bd445f9e504cea36ad9f37b52dda7b4a1
object-Object <object@objectobject.ca>
2024-11-16 16:33:24 -0500

    Rename test.s to math.s

---
+++ programs/math.s
@@ -0,0 +1,7 @@
+.global _start
+_start:
+    li x1, -1
+    li x2, 2
+    li x3, 2
+    slti x2, x1, 1
+    sltiu x3, x1, 1
--- programs/test.s
+++
@@ -1,6 +0,0 @@
-.global _start
-_start:
-    addi x1, x0, 10
-    addi x1, x1, -1
-    li x2, 60
-    add x3, x1, x2
